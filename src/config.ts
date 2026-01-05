6aa2878414682ac85da1b58ce6e7de197879df50
bradleydfrank <wtqnngjmzn@privaterelay.appleid.com>
2026-01-05 16:07:12 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "bradleydfrank.github.io", // replace this with your deployed domain
+  author: "Bradley D. Frank",
+  profile: "bradleydfrank.github.io",
+  desc: "description",
+  title: "title",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
